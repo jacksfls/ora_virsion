80af38b7546545089a07bd23439c8fcfb11498f7
jiechen <jiechen@simbo.com.tw>
2018-10-24 14:58:27 +0800

    test.sql add db_user

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
-create table emp8 select * from emp6 where 1=0;
-insert into emp8 as select * from emp6 ;
+#db_user=game
+
+create table emp8 select * from emp1 where 1=0;
+insert into emp8 as select * from emp1 ;
 	commit;
